f086fbf977b520be54c38ad404481dbe56ef730d
Leqi(Nancy) Wan <leqiwan@alumni.ubc.ca>
2019-11-28 20:15:14 -0800

    some efforts in getting two spiders written to the same csv file using flask

--- bike_crawler/index.py
+++ bike_crawler/index.py
@@ -1,6 +1,8 @@
 import subprocess
-
+import json
+from scrapy.crawler import CrawlerRunner
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,10 +14,26 @@ def index():
 
     wait for  this command to finish, and read output.json to client.
     """
-    spider_name = "bike1_spider"
-    subprocess.check_output(['scrapy', 'crawl', spider_name, "-o", "bikecrawler.json"])
-    with open("bikecrawler.json") as items_file:
-        return items_file.read()
+    spider_names = ["bike1_spider", "bike2_spider"]
+    data = {'bike_name': [], 'price': [], 'site_url': []}
+    for spider_name in spider_names:
+        subprocess.check_output(['scrapy', 'crawl', spider_name, "-o", "bikecrawler.json"])
+        items_file = open("bikecrawler.json", "r").read()
+        # print(items_file)
+        for line in items_file:
+            for key in data.keys():
+                data[key] = list(data[key])
+                data[key].append(line[key])
+        # items_file.close()
+    
+    with open('bikecrawler.json', 'w') as f:
+        json.dump(data, f)
+        # for d in data:
+        #     d_str = ''.join(str(d))
+        #     print(d_str)
+        #     # json_acceptable_str = d_str.replace("'", "\"")
+        #     f.write(json.dumps(json.loads(d_str)))
+        return f
 
 if __name__ == '__main__':
     app.run(debug=True)
--- bike_crawler/pipelines.py
+++ bike_crawler/pipelines.py
@@ -8,6 +8,8 @@ from scrapy import signals
 from scrapy.exporters import JsonItemExporter
 
 class BikeCrawlerPipeline(object):
+    def __init__(self):
+        self.files = {}
     @classmethod
     def from_crawler(cls, crawler):
         pipeline = cls()
@@ -16,13 +18,13 @@ class BikeCrawlerPipeline(object):
         return pipeline
 
     def spider_opened(self, spider):
-        file = open('bikecrawler.json', 'wb')
-        self.exporter = JsonItemExporter(file)
+        self.file = open('bikecrawler.json', 'wb')
+        self.exporter = JsonItemExporter(self.file)
         self.exporter.start_exporting()
 
     def spider_closed(self, spider):
         self.exporter.finish_exporting()
-        file.close()
+        self.file.close()
 
     def process_item(self, item, spider):
         self.exporter.export_item(item)
